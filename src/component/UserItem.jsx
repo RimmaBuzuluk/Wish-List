83fc6f566749e63bc336620f3ffd116d4c1ea5ef
RimmaBuzuluk <rimmysya2000@gmail.com>
2023-07-20 20:13:41 +0300

    chackMark

--- src/component/UserItem.jsx
+++ src/component/UserItem.jsx
@@ -2,10 +2,18 @@ import { observer } from 'mobx-react';
 import '../App.css';
 import ButBackTextWish from '../component/ButBacktTextWish';
 import PersonStore from '../store/PersonStore';
+import UserStore from '../store/UserStore';
 
 const UserItem = observer(({person}) =>{
+
+  const isCurrentUser=person.id===UserStore.currentUser.id
+
+  console.log(UserStore.currentUser)
   return (
     <div className='UserItem'>
+      <div className='checkMarkContainer'>
+          {isCurrentUser && <div className='checkMark'>*</div>}
+       </div>
         <div>
             <img src={person.photo}/>
         </div>
